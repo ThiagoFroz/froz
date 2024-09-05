adf800620d8ef31aa89e0f9a734cf913b93027f6
ThiagoFroz <104405393+ThiagoFroz@users.noreply.github.com>
2024-09-05 02:58:36 +0000

    ajustando parametros

---
+++ public/script.js
@@ -0,0 +1,16 @@
+async function carregarSugestoes() {
+    const response = await fetch('/.netlify/functions/get-suggestions');
+    const sugestoes = await response.json();
+    
+    const listaSugestoes = document.getElementById('sugestoes');
+    listaSugestoes.innerHTML = '';
+  
+    sugestoes.forEach(sugestao => {
+      const li = document.createElement('li');
+      li.textContent = `${sugestao.nome}: ${sugestao.sugestao}`;
+      listaSugestoes.appendChild(li);
+    });
+  }
+  
+  // Carregar sugestões quando a página é carregada
+  window.onload = carregarSugestoes;
